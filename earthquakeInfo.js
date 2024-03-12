4fca91ea1d91fa5f2c7ca8eb37f1f2b0a185f705
ChaoHsaun <162970269+ChaoHsaun@users.noreply.github.com>
2024-03-12 23:19:14 +0800

    Update earthquakeInfo.js

--- earthquakeInfo.js
+++ earthquakeInfo.js
@@ -3,7 +3,7 @@ const cheerio = require('cheerio');
 
 async function fetchEarthquakeInfo() {
   try {
-    const response = await axios.get('https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization=CWA-E7806F72-E26F-4236-8250-055D6048C4C3&limit=30&format=JSON&AreaName=%E8%87%BA%E5%8D%97%E5%B8%82');
+    const response = await axios.get('https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization=');
     const html = response.data;
     const $ = cheerio.load(html);
 
